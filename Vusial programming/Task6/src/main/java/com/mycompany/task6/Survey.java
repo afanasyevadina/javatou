af95cda4f8c78c83ac0d153c46a1f2cad775aa18
afanasyevadina <da@tecedu.org>
2021-10-24 21:21:27 +0600

    Task 6

---
+++ Vusial programming/Task6/src/main/java/com/mycompany/task6/Survey.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.task6;
+
+/**
+ *
+ * @author Dina-PC
+ */
+public class Survey {
+    String childhood;
+    String adolescence;
+    String youth;
+   
+    void setChildhood(String childhood)
+    {
+        this.childhood = childhood;
+    }
+   
+    void setAdolescence(String adolescence)
+    {
+        this.adolescence = adolescence;
+    }
+   
+    void setYouth(String youth)
+    {
+        this.youth = youth;
+    }
+   
+    String getChildhood()
+    {
+        return this.childhood;
+    }
+   
+    String getAdolescence()
+    {
+        return this.adolescence;
+    }
+   
+    String getYouth()
+    {
+        return this.youth;
+    }
+}
